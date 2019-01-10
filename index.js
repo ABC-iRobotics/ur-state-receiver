e0953d7ccd0450850b47f321ba31c8f8455a8d41
Matyas Takacs <matyas.takacs@irob.uni-obuda.hu>
2019-01-10 11:13:16 +0100

    auto reconnect feature added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const urStateDef = require('./urStateDef.js')
 const client = new net.Socket()
 
 var currentState = {}
+var retrying = false
+const timeOut = 5000
 
 client.on('data', function (data) {
   for (var key in urStateDef) {
@@ -17,13 +19,26 @@ client.on('data', function (data) {
 class URState extends EventEmitter {
   constructor (PORT, HOST) {
     super()
-    client.connect(PORT, HOST, () => { this.emit('connect') })
+    client.connect(PORT, HOST, () => {
+      this.emit('connect')
+      retrying = false
+    })
     client.on('data', () => { this.emit('data', currentState) })
     client.on('error', (exception) => { this.emit('exception', exception) })
     client.on('drain', () => { this.emit('drain') })
     client.on('timeout', () => { this.emit('timeout') })
-    client.on('close', () => { this.emit('close') })
+    client.on('close', () => {
+      if (!retrying) {
+        retrying = true
+        console.log("Reconnecting...")
+      }
+      setTimeout(() => {
+        client.connect(PORT, HOST)
+      }, timeOut)
+      this.emit('close')
+    })
   }
 }
 
 module.exports = URState
+
