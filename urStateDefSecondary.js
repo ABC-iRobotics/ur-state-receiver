f39a5992976c8ecf0a38e04518b3b8dd0846e3f2
maystery <emodi.mark@gmail.com>
2019-10-22 10:47:26 +0200

    Remove nodejs code

--- urStateDefSecondary.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  actJ1pos: {start: 64, length: 8, description: 'Actual joint 1 position'},
-  actJ2pos: {start: 105, length: 8, description: 'Actual joint 2 position'},
-  actJ3pos: {start: 146, length: 8, description: 'Actual joint 3 position'},
-  actJ4pos: {start: 187, length: 8, description: 'Actual joint 4 position'},
-  actJ5pos: {start: 228, length: 8, description: 'Actual joint 5 position'},
-  actJ6pos: {start: 269, length: 8, description: 'Actual joint 6 position'}
-}
